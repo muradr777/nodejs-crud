2b4e86d9383751a0ecfcba638354db6a17e1302c
Murad <muradrustamov@gmail.com>
2018-12-02 14:27:17 +0400

    Connected to the server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create Express app
+const app = express();
+const port = 3000;
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({'message': 'Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.'});
+});
+
+app.listen(port, () => {
+    console.log(`Listening port ${port}`);
+});
